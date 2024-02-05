6f492e61040c06747f31f4b6816135dbec33cebc
Babyr <157361024+Kozhantay@users.noreply.github.com>
2024-02-05 21:49:12 +0600

    Add files via upload

---
+++ labka 1/type.py
@@ -0,0 +1,45 @@
+#Exercise 1
+x = 5
+print(type(x))
+
+int
+
+#Exercise 2
+x = "Hello World"
+print(type(x))
+
+str
+
+#Exercise 3
+x = 20.5
+print(type(x))
+
+float
+
+#exercise 4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+list
+
+#exercise 5
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+tuple
+
+#exercise 6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+dict
+
+#Exercise 7
+x = True
+print(type(x))
+
+bool
+
+
+
+
